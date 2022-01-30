f77d95123e3cd18e2745ff5069ffc3536fd5f210
Ben Speakman <ben@3sq.re>
2022-01-30 17:20:22 +0000

    Fixes

--- infra/main.go
+++ infra/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"github.com/pulumi/pulumi-aws/sdk/go/aws"
-	"github.com/pulumi/pulumi-aws/sdk/go/aws/acm"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
@@ -17,17 +15,6 @@ func main() {
 		if err != nil {
 			return err
 		}
-
-		domain := pulumi.String("chatsh.it")
-		awsUsEast1, err := aws.NewProvider(ctx, "aws-provider-us-east-1", &aws.ProviderArgs{Region: pulumi.String("us-east-1")})
-		sslCertificate, err := acm.NewCertificate(ctx,
-			"ssl-cert",
-			&acm.CertificateArgs{
-				DomainName:       domain,
-				ValidationMethod: pulumi.String("DNS"),
-			},
-			pulumi.Provider(awsUsEast1),
-		)
 
 		ctx.Export("url", gateway.Url)
 
